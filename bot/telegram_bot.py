5b43d9e67ed255421b376cb466369d45216cbe5c
Dglockersup <caiphe7@gmail.com>
2026-03-26 10:26:52 +0100

    Mise a jour du token

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -4,7 +4,7 @@ TOKEN = "8623082732:AAHoxYTdqTkRvDCxEms0jdEZNT2sS0peapc"
 CHAT_ID = "6379592530"
 
 def send(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot8623082732:AAHoxYTdqTkRvDCxEms0jdEZNT2sS0peapc/sendMessage"
 
     data = {
         "chat_id": CHAT_ID,
